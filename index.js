4ddc55cb48d974b3eafe0d3c997bcd3e55b17493
Brooke Strand <brooke.strand007@gmail.com>
2022-02-28 21:46:08 -0500

    commit tree 75c61d603ccc5efa75b1daa0a157dc458dbdcbe9

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    word.toLowerCase();
+    word.toUpperCase(word[0]);
+  }
+
+  static sanitize(string) {
+     let fixedIt = string.replace(/[^-,'A-Za-z0-9]+/g, '');
+      return fixedIt;
+    }
+
+  static titleize(string) {
+    string = string.toLowerCase().split(' ');
+    for (let i = 0; i < string.length; ++i) {
+      string[i].charAt(0).toUpperCase() + string[i].slice(1);
+    }
+      return string.join(' ');
+  }
 }
